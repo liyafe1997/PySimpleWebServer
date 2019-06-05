af122f539f47bba89844cba6f7bd683396794ccb
yanshengheng <awesomerandy@hotmail.com>
2019-06-05 23:52:44 +0800

    add delete friendly link

--- webserver.py
+++ webserver.py
@@ -229,25 +229,42 @@ class S(BaseHTTPRequestHandler):
                 self.wfile.write(("failed").encode())
         #end of delete link
 
-        #function 16:
+        #function 15:
         #/deletecomment?adminuser=42432&adminpassword=fadsfads&commentid=1
+        #function 16:
+        # /deletelink?adminuser=42432&adminpassword=fadsfads&linkid=1&linkname=xxx&linkurl=xxx&orderno=xxx
         elif node == "/deletecomment"
             queryString = "SELECT password FROM m_account WHERE username="+str(requests["adminuser"][0])
             result = SQLike_Query(queryString)
             result=result.split('\n')[1].split('\t')[0] #Get the second line(first line is colonm) and the first data
-            #--verifing username password
-            if result == requests["adminpassword"][0]: #password matched
-                print "Delete blog admin succeed!"
-                try:
-                    print ("Delete tblog : "+SQLike_Query("DELETE t_comment WHERE id="+str(requests["commentid"][0])))
-                    self.wfile.write(("ok").encode())
-                except:
+            
+            if requests["orderno"][0] == None:
+                #--verifing username password
+                if result == requests["adminpassword"][0]: #password matched
+                    print "Delete blog admin succeed!"
+                    try:
+                        print ("Delete tblog : "+SQLike_Query("DELETE t_comment WHERE id="+str(requests["commentid"][0])))
+                        self.wfile.write(("ok").encode())
+                    except:
+                        self.wfile.write(("failed").encode())
+                else:
+                    print "Comment Process Login failed"
                     self.wfile.write(("failed").encode())
             else:
-                print "Comment Process Login failed"
-                self.wfile.write(("failed").encode())
-        #end of delete comment
+                if result == requests["adminpassword"][0]: #password matched
+                    print "Delete blog admin succeed!"
+                    try:
+                        #delete friendly link
+                        print ("Delete tblog : "+SQLike_Query("DELETE t_comment WHERE id="+str(requests["commentid"][0])))
+                        self.wfile.write(("ok").encode())
+                    except:
+                        self.wfile.write(("failed").encode())
+                else:
+                    print "Comment Process Login failed"
+                    self.wfile.write(("failed").encode())
+        #end of delete comment & friendly link
 
+        
         elif node == "/keywordsearch":
             queryString = "SELECT id,title,typeId,releaseDate,clickHit FROM tblog WHERE keyWord="+str(requests["keyword"][0])  
             result = SQLike_Query(queryString)
@@ -329,6 +346,9 @@ class S(BaseHTTPRequestHandler):
                 self.wfile.write(("ok").encode())
             except:
                 self.wfile.write(("failed").encode())
+
+        #function 9
+        #/blogmanage?username=xxx&password=123&nickname=xxx&sign=xxx&profile=xxx&imagename=xxx&blogtype=xxx
         elif node =="/blogmanage":
             #verifing username password
             queryString = "SELECT password FROM u_account WHERE userName="+str(requests["username"][0])
@@ -395,6 +415,7 @@ class S(BaseHTTPRequestHandler):
                 self.wfile.write(("ok").encode()) 
             except:
                 self.wfile.write(("failed").encode())
+
         elif node =="/comment":
         #verifing username password
             queryString = "SELECT password FROM u_account WHERE userName="+str(requests["username"][0])
@@ -448,6 +469,8 @@ class S(BaseHTTPRequestHandler):
             else:
                 print "Comment Process Login failed"
                 self.wfile.write(("failed").encode())
+
+        #function 12
         elif node =="/deleteuser":
             #/deleteuser?adminuser=42432&adminpassword=fadsfads&userid=xxx
 
